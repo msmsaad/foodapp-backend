2c59a9066d4243c36111d05fa8c44d6a3a1810a3
saad <saad.msm786@gmail.com>
2023-07-24 20:21:26 +0500

    schema.rb updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :database_authenticatable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
