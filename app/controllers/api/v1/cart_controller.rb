e4c7ed8413a352be85008540f05c3bf3a77cf322
saad <saad.msm786@gmail.com>
2023-07-25 14:24:16 +0500

    carts controller updating the nested carts meals

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -4,11 +4,28 @@ module Api
   module V1
     class CartController < BaseController
       before_action :authenticate_user!
+      before_action :find_cart
 
       def show
+        render json: @cart
       end
 
       def update
+        if @cart.update(cart_params)
+          render json: @cart
+        else
+          render json: { errors: @cart.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def cart_params
+        params.require(:carts_meals).permit(carts_meals_attributes: [:id, :meal_id, :quantity, :_destroy])
+      end
+
+      def find_cart
+        @cart = current_user.cart || Cart.new(user: current_user)
       end
     end
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,6 @@ class Cart < ApplicationRecord
   belongs_to :user
 
   has_many :carts_meals, dependent: :destroy
+
+  accepts_nested_attributes_for :carts_meals, allow_destroy: true
 end
