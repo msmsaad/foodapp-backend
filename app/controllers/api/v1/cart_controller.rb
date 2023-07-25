ba490b382744ab291493f537354516ddeb0eb8cb
saad <saad.msm786@gmail.com>
2023-07-25 12:40:59 +0500

    cart controller added

---
+++ app/controllers/api/v1/cart_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CartController < BaseController
+      before_action :authenticate_user!
+
+      def show
+
+      end
+
+      def update
+        
+      end
+    end
+  end
+end
