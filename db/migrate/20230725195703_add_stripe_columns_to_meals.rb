0cc22bfae9d8ee0893823d75b8cb74cb4480493c
saad <saad.msm786@gmail.com>
2023-07-26 00:58:11 +0500

    stripe id columns added to meal

---
+++ db/migrate/20230725195703_add_stripe_columns_to_meals.rb
@@ -0,0 +1,8 @@
+class AddStripeColumnsToMeals < ActiveRecord::Migration[7.0]
+  def change
+    change_table :meals, bulk: true do |t|
+      t.string :stripe_product_id
+      t.string :stripe_price_id
+    end
+  end
+end
