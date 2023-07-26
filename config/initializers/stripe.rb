a15eb1f9b241a05b9ae4a7f78f041beba3d8471a
saad <saad.msm786@gmail.com>
2023-07-26 15:59:35 +0500

    categories controller request tests added

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Stripe.api_key = FoodappBackend::Application.credentials[Rails.env.to_sym][:stripe][:secret_key]
+unless Rails.env.test?
+  Stripe.api_key = FoodappBackend::Application.credentials[Rails.env.to_sym][:stripe][:secret_key]
+end
