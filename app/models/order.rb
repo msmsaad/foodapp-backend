ca9c55bdd8efe96148b99d8ce289c403b47cf28a
saad <saad.msm786@gmail.com>
2023-07-26 13:29:41 +0500

    webhook controller with stripe webhook secrect key in the creads

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,7 @@ class Order < ApplicationRecord
   belongs_to :user
 
   has_many :ordered_meals, dependent: :destroy
+
+  validates :stripe_payment_id, presence: true
+  validates :stripe_payment_id, uniqueness: true
 end
