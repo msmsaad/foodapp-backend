5ae8cce945c2b3eeac53ab0dee52088508c6f154
saad <saad.msm786@gmail.com>
2023-07-27 23:27:21 +0500

    small change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # TODO: sidekiq should be accessible behind the admin panel login
   mount Sidekiq::Web => "/admin/sidekiq"
 
-  mount_devise_token_auth_for "User", at: "auth"
+  mount_devise_token_auth_for "User", at: "api/auth"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
